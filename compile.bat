489ddb3432ab6a2a89ab963955fedc6a2f951b7d
wangzhiyan <wangzhiyan@megarobo.tech>
2018-12-27 18:43:41 +0800

    merge

--- compile.bat
+++ compile.bat
@@ -5,8 +5,9 @@ cd .
 
 rd /s /q build
 rd /s /q dist
+rd /s /q installer
 pyinstaller t4app.spec
 
-xcopy dist\MRX-T4 installer /e /h
+xcopy dist\MRX-T4 installer\ /e /h
 copy t4app.iss installer
 pause
